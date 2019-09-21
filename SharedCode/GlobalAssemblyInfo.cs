309ed43b7edebd6c666ece2dcd7b7c11e903e21f
Tim Long <Tim@tigranetworks.co.uk>
2019-09-21 05:34:57 +0100

    Automatic versioning based on  GitVersion

---
+++ SharedCode/GlobalAssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿// This file is part of the TA.WinForms.Controls project
+// Copyright © 2016-2019 Tigra Astronomy, all rights reserved.
+// File: AssemblyInfo.cs  Last modified: 2019-09-21@02:42 by Tim Long
+// Licensed under the Tigra MIT License, see https://tigra.mit-license.org/
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tigra Astronomy")]
+[assembly: AssemblyProduct("TA.WinFormsControls library")]
+[assembly: AssemblyCopyright("Copyright © 2006-2019 Tigra Astronomy, all rights reserved")]
+[assembly: AssemblyTrademark("Tigra Astronomy")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
--- TA.WinFormsControls.TestApplication/Properties/AssemblyInfo.cs
+++ TA.WinFormsControls.TestApplication/Properties/AssemblyInfo.cs
@@ -1,36 +1,9 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿// This file is part of the TA.WinForms.Controls project
+// Copyright © 2016-2019 Tigra Astronomy, all rights reserved.
+// File: AssemblyInfo.cs  Last modified: 2019-09-21@05:23 by Tim Long
+// Licensed under the Tigra MIT License, see https://tigra.mit-license.org/
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+using System.Reflection;
+
 [assembly: AssemblyTitle("TA.WinFormsControls.TestApplication")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TA.WinFormsControls.TestApplication")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d39891b1-ede6-4388-9f89-b8b73ac3b458")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("A simple static test application for the controls")]
